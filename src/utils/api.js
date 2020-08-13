fcb3ed5a4f4bb55e36216f0c68cea298baaadc30
Parreno <847914@cognizant.com>
2020-08-13 12:18:53 +0200

    Finally I fixed the CORS problem for development

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,9 @@
 import Axios from "axios";
 
-const marvelURL = 'proxy/v1/public',
-    apiKey = `apikey=${process.env.REACT_APP_MARVEL_PUBLIC_API_KEY}`;
+  const  apiKey = `apikey=${process.env.REACT_APP_MARVEL_PUBLIC_API_KEY}`;
 
 export const getCharacters = (options) => {
-    return Axios.get(`${marvelURL}/characters/?${apiKey}`)
+    return fetch(`http://gateway.marvel.com/v1/public/characters?${apiKey}`)
         .then(res => {
             const characters = res.results;
             return characters;
