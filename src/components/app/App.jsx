9450b6dc1616fd4f74420c7b163e9c04ac8f67b3
Parreno <847914@cognizant.com>
2020-08-28 18:28:15 +0200

    Added navigation on pressing Marvel logo

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,17 +4,24 @@ import { displayedCharacters, searchTextState } from "../../state";
 import MarvelButton from '../button/MarvelButton';
 import Dashboard from '../dashboard/Dashboard';
 import SearchBar from '../search-bar/SearchBar';
+import { useHistory } from "react-router-dom";
 import './App.css';
 
 
 const App = () => {
 
+  const history = useHistory();
   const text = useRecoilValue(searchTextState);
   const [heroes, setDisplayedCharacters] = useRecoilState(displayedCharacters);
+
+  const _navigateHome = () => {
+    history.push(`/`);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
+        <img className="logo" src="/1920px-MarvelLogo.svg.png" onClick={_navigateHome}></img>
         <MarvelButton text="search" searchText={text} onClickedAction={setDisplayedCharacters}></MarvelButton>
         <SearchBar text={text}></SearchBar>
       </header>
@@ -24,6 +31,8 @@ const App = () => {
       </footer>
     </div>
   );
+
+
 }
 
 export default App;
