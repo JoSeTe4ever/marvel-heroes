4cc379d80999da2335d27b27a493ab1299eb5d30
Parreno <847914@cognizant.com>
2020-09-24 17:50:38 +0200

    Updates

---
+++ precache-manifest.427aa54b083a2680ba1d9e99e6483446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a361f79952f4475a84d4b5c7c7ef073",
+    "url": "/marvel-heroes/index.html"
+  },
+  {
+    "revision": "099181b612c9cb26b2e8",
+    "url": "/marvel-heroes/static/css/main.d391edea.chunk.css"
+  },
+  {
+    "revision": "3795f1e1eb7a52f97d4b",
+    "url": "/marvel-heroes/static/js/2.60ff81c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marvel-heroes/static/js/2.60ff81c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099181b612c9cb26b2e8",
+    "url": "/marvel-heroes/static/js/main.ca50bb66.chunk.js"
+  },
+  {
+    "revision": "7826c1f5f1eaf95ebe2e",
+    "url": "/marvel-heroes/static/js/runtime-main.b48286e9.js"
+  }
+]);
