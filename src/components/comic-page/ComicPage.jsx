4b6fd4e067645d42471e483e87cde7d13d98af12
josete4ever <josete4ever@gmail.com>
2023-07-26 23:45:12 +0200

    feat: added comic-page wip

---
+++ src/components/comic-page/ComicPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const ComicPage = () => {
+    return (
+        <div className="comic__container">
+            <div className="row__container" style="background-color:red">
+                <div className="row__strip30">
+                </div>
+                <div className="row__strip70">
+                </div>
+            </div>
+            <div className="row">
+                <div className="row__strip70">
+                </div>
+                <div className="row__strip30">
+                </div>
+            </div>
+            <div className="row">
+                <div className="row__strip70">
+                </div>
+                <div className="row__strip30">
+                </div>
+            </div>
+        </div>
+    )
+}
