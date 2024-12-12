8a8be225fca04b4ad0e97c98b710caaf8fe5b203
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2024-12-12 14:18:04 +0100

    fix: added background

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,14 +1,17 @@
-import { default as React } from 'react';
-import { ComicPage } from '../comic-page/ComicPage';
-import './Home.css';
+import { default as React } from "react";
+import { ComicPage } from "../comic-page/ComicPage";
+import "./Home.css";
 
 export const Home = () => {
-
-
-    return (
-        <div className="home">
-            <ComicPage></ComicPage>
-        </div>
-    )
-
-}
+  return (
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url("/img/4851275.jpg")`,
+        backgroundSize: 'cover'
+      }}
+    >
+      <ComicPage></ComicPage>
+    </div>
+  );
+};
